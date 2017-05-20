31d79a4aeb9eaec68f3fba5170b2edf1ef8d7ab2
giordano henri <henri.giordano@gmail.com>
2017-05-20 11:58:46 +0200

    ajout commetaire

--- core/src/main/java/pro/jtaylor/timetracker/core/Tracker.java
+++ core/src/main/java/pro/jtaylor/timetracker/core/Tracker.java
@@ -10,19 +10,31 @@ import java.util.List;
 public class Tracker {
     @Autowired
     private List<TimeEntry> entries;
-
+    /*method add
+    *@paremtre 
+    *
+    */
     public void add(TimeEntry entry) {
         entries.add(entry);
     }
-
+   /*method add
+    *@paremtre 
+    *
+    */
     public void remove(TimeEntry entry) {
         entries.remove(entry);
     }
-
+   /*method add
+    *@paremtre 
+    *
+    */
     public int size() {
         return entries.size();
     }
-
+   /*method add
+    *@paremtre 
+    *
+    */
     public TimeEntry get(int index) {
         return entries.get(index);
     }
